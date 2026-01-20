8ef6ca89c93c527f9cff5e19b1a9564f75bab75e
pathlib <gautroncyber@proton.me>
2026-01-20 16:43:46 +0000

    creation du socket

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import socket
 #ce code n,as pas été coder par une ia
-v=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serveur =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+HOST = "123.0.0.1"
+PORT = 5001
+serveur.bind((HOST,PORT))
+serveur.listen()
